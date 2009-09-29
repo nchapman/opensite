08dfb8ffa37027c14d9e68d670b41b5b1ee32f35
Nick Chapman <nchapman@gmail.com>
2009-09-28 22:42:02 -0700

    Added validations to Person.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,14 @@
 class Person < ActiveRecord::Base
+  
+  attr_reader :password
+  
+  validates_presence_of :first_name, :last_name, :email
+  validates_format_of :email, :with => /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+  validates_uniqueness_of :email
+  validates_confirmation_of :password
+  
+  def password=(password)
+    @password = password
+  end
+  
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,16 +2,54 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Person do
   before(:each) do
+    @person = Person.new
+    
     @valid_attributes = {
-      :first_name => "value for first_name",
-      :last_name => "value for last_name",
-      :email => "value for email",
-      :salt => "value for salt",
-      :encrypted_password => "value for encrypted_password"
+      :first_name => "John",
+      :last_name => "Smith",
+      :email => "jsmith@example.com",
+      :password => "password",
+      :password_confirmation => "password"
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    Person.create!(@valid_attributes)
+  it "should be valid" do
+    @person.attributes = @valid_attributes
+    @person.should be_valid
+  end
+  
+  it "should validate presence of first name" do
+    @person.attributes = @valid_attributes.except(:first_name)
+    @person.should have(1).error_on(:first_name)
+  end
+  
+  it "should validate presence of last name" do
+    @person.attributes = @valid_attributes.except(:last_name)
+    @person.should have(1).error_on(:last_name)
+  end
+  
+  it "should validate presence of email" do 
+    @person.attributes = @valid_attributes.except(:email)
+    @person.should have(2).error_on(:email)
+  end
+  
+  it "should require a valid email" do
+    @person.attributes = @valid_attributes.with(:email => "jsmithexample.com")
+    @person.should have(1).error_on(:email)
+    
+    @person.attributes = @valid_attributes.with(:email => "jsmith@examplecom")
+    @person.should have(1).error_on(:email)
+  end
+  
+  it "should require a unique email" do
+    @another_person = Person.create!(@valid_attributes)
+    @person.attributes = @valid_attributes
+    
+    @person.should have(1).error_on(:email)
+  end
+  
+  it "should require password confirmation" do
+    @person.attributes = @valid_attributes.with(:password_confirmation => "paZZword")
+    @person.should have(1).error_on(:password)
   end
 end
