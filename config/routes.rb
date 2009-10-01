89e1bb00564c0c047934aeb780d91686e00be2e4
Nick Chapman <nchapman@gmail.com>
2009-10-01 07:31:05 -0700

    Fixed user session routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
-  map.resource :account, :controller => "users"
   map.resources :user_sessions
+  map.login "/login", :controller => "user_sessions", :action => "new"
+  map.logout "/logout", :controller => "user_sessions", :action => "destroy"
+  map.resource :account, :controller => "users"
   
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   map.connect "products/:id", :controller => "catalog", :action => "view"
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   map.purchase "products/:id/purchase", :controller => "catalog", :action => "purchase"
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
@@ -41,7 +43,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # consider removing or commenting them out if you"re using named routes and resources.
+  map.connect ":controller/:action/:id"
+  map.connect ":controller/:action/:id.:format"
 end
